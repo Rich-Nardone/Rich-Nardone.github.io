3061cc896cf449c527be0691ee811854c7f72908
Rich Nardone <rmn9@njit.edu>
2020-12-06 16:44:04 +0000

    Set up to pull from main

--- scripts/ClientID.jsx
+++ scripts/ClientID.jsx
@@ -1,6 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-let ClientID = "656111270790-6jsfgnirr63rvkth2ro0u35l4alkugrg.apps.googleusercontent.com"
-
+let ClientID = "441337686833-afmhrf35a9l72dui53r94ggtgja42me3.apps.googleusercontent.com"
 
 export {ClientID};
 
